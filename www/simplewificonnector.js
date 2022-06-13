ee58fe9119e5c6e19a782b9da68c9188d8ae739c
Martin Selbmann <job@martin-selbmann.de>
2022-06-13 13:59:40 +0200

    Update simplewificonnector.js

--- www/simplewificonnector.js
+++ www/simplewificonnector.js
@@ -1,7 +1,6 @@
 var exec = require('cordova/exec');
 
 exports.connect = function (arg0, arg1) {
-    console.log("called connect with params:",arg0+","+arg1)
     return new Promise(function (resolve, reject) {
         exec(resolve, reject, 'simplewificonnector', 'connect', [arg0,arg1]);
     });
